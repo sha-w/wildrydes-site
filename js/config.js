87da8a0a508b5c750625bfa381e6533fd971423e
sha-w <mike@castlefortress.co>
2019-10-29 11:02:55 -0500

    configureed cognito II

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_AKjpm0lmh', // e.g. us-west-2_AKjpm0lmh
         userPoolClientId: '62jv82k8r4o7d7u7lrf2j4u7tp', // e.g. 62jv82k8r4o7d7u7lrf2j4u7tp
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
